832b4643685ceafabf1528683cb1f4f67e49c045
adventurous-class-0665 <rngao97@gmail.com>
2019-08-14 19:13:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  var lowercase = string;
-  if (lowercase.toLowerCase === lowercase){
+  if (string === string.toLowerCase){
     return "I can't hear you!"
   }
   else {
